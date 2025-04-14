842a209e52b58287685fc52988ab0ce91059383f
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2025-04-14 21:51:31 +0700

    Update TỔNG LỚN NHẤT CỦA DÃY CON KHÔNG KỀ NHAU.cpp

--- TỔNG LỚN NHẤT CỦA DÃY CON KHÔNG KỀ NHAU.cpp
+++ TỔNG LỚN NHẤT CỦA DÃY CON KHÔNG KỀ NHAU.cpp
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         prev2 = a[0]
         prev1 = max(a[0], a[1])
         for i in range(2, n):
-            current = max(prev2 + a[i], prev1)
+            current = max(prev2 + a[i], prev1) #dp[i] = (dp[i - 2] + a[i], dp[i - 1]) // dp[i - 2] là lấy a[i], dp[i - 1] là không lấy
             prev2, prev1 = prev1, current
         
         print(prev1)
