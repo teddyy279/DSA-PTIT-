d88915fff9b081091473b2b8a583cefc0403a3da
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2025-05-12 09:38:27 +0700

    Update F. Thư viện thành phố.cpp

--- F. Thư viện thành phố.cpp
+++ F. Thư viện thành phố.cpp
@@ -47,3 +47,61 @@ int main(){
     }
     cout << total_cost << "\n";
 }
+/*
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int n, m;
+int parent[100005];
+int sz[100005];
+int price[100005];
+int min_cost[100005];
+
+void init(){
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        parent[i] = i;
+        sz[i] = 1;
+        cin >> price[i];
+        min_cost[i] = price[i];
+    }
+}
+
+int Find(int u){
+    if(u == parent[u]) return u;
+    return parent[u] = Find(parent[u]);
+}
+
+bool Union(int u, int v){
+    int root_u = Find(u);
+    int root_v = Find(v);
+    if(root_u == root_v) return false;
+    if(sz[root_u] < sz[root_v]){
+        swap(root_u, root_v);
+    }
+    sz[root_u] += sz[root_v];
+    parent[root_v] = root_u;
+    min_cost[root_u] = min(min_cost[root_u], min_cost[root_v]);
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    for(int i = 0; i < m; i++){
+        int x, y; cin >> x >> y;
+        Union(x, y);
+    }
+    ll total_cost = 0;
+    for(int i = 1; i <= n; i++){
+        if(Find(i) == i){
+            total_cost += min_cost[i];
+        }
+    }
+    cout << total_cost << "\n";
+}
+*/
